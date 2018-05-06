e646a521d20733372214ee3fd6a054554ead0389
Neil Donewar <neil@donewar.com>
2018-05-05 21:00:25 -0400

    Well, let's get started
    - Clean up HTML with some basic HTML5
    - Move the CSS and JS into their own files
    - Run [standard](https://standardjs.com/) on the JS to save time
    - Get to zero warnings in WebStorm

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# samegame
+# SameGame
+
+About 10 years ago (2008), I was bored one night and threw together a quick implementation of <a href="https://en.wikipedia.org/wiki/SameGame">SameGame</a>. It's a tile-matching puzzle game I played (way too much) on Windows 95.
+
+The code is... rough. But the game is playable. I found it recently and thought it might be fun to revisit and refactor a little.
+
+> Side note: I don't know the exact date I wrote the code. The file modification date isn't correct. However, I can "carbon date" the file to early 2008 based on the jQuery version. The game used version 1.2.3, which was [released](https://blog.jquery.com/2008/02/07/jquery-1-2-3-released/) on Feb. 7, 2008. The next version was [released](http://blog.jquery.com/2008/05/20/jquery-1-2-4-released/) on May 20, 2008.
+
+## Refactoring log:
+
+Well, let's get started
+- Clean up HTML with some basic HTML5
+- Move the CSS and JS into their own files
+- Run [standard](https://standardjs.com/) on the JS to save time
+- Get to zero warnings in WebStorm
---
+++ js/app.js
@@ -0,0 +1,128 @@
+/* global $ */
+
+const colors = ['Red', 'Blue', 'Green', 'Purple']
+const bX = 31
+const bY = 19
+let points = 0
+let total = 0
+function generatePieces () {
+  for (let i = 0; i < bX * bY; i++) {
+    const pTop = Math.floor(i / bX) * 40
+    const pLeft = (i % bX) * 40
+    const color = colors[Math.floor(Math.random() * colors.length)]
+    $('<div class="p"></div>').attr('id', i).css('top', pTop).css('left', pLeft).css('background-color', color).appendTo('#board').click(
+      function () {
+        if (isMarked(this)) {
+          deleteMarked()
+          settleDown()
+          settleLeft()
+          updateTotal(points)
+        } else {
+          $('.mark').removeClass('mark')
+          let n = checkTRBL(this)
+          points = (n - 1) * (n - 1)
+          $('#points').html('' + points)
+          if (n) {
+            mark(this)
+          } else {
+            updatePoints(0)
+          }
+        }
+      }
+    )
+  }
+}
+generatePieces()
+
+function updatePoints (n) {
+  points = n
+  $('#points').html(n)
+}
+
+function updateTotal () {
+  total += points
+  $('#total').html('' + total)
+  updatePoints(0)
+}
+
+function settleLeft () {
+  let again = false
+  for (let h = 0; h < bX - 1; h++) {
+    const col = []
+    for (let v = 0; v < bY; v++) {
+      const id = h + (bX * v)
+      if (document.getElementById(id.toString())) {
+        break
+      } else {
+        col.push(id)
+        if (col.length === bY) {
+          while (col.length > 0) {
+            const oid = col.pop()
+            if (document.getElementById(oid + 1)) {
+              again = true
+              $('#' + (oid + 1)).attr('id', oid).animate({left: '-=40'}, 10)
+            }
+          }
+        }
+      }
+    }
+  }
+  if (again) settleLeft()
+}
+
+function settleDown () {
+  let again = false
+  for (let i = bX * bY - 1; i > -1; i--) {
+    if (!document.getElementById(i.toString())) {
+      if (document.getElementById((i - bX).toString())) {
+        $('#' + (i - bX)).attr('id', i).animate({top: '+=40'}, 10)
+        again = true
+      }
+    }
+  }
+  if (again) settleDown()
+}
+
+function deleteMarked () {
+  $('.mark').remove()
+}
+
+function mark (el) {
+  $(el).addClass('mark')
+}
+
+function checkTRBL (el) {
+  let c = 0
+  const t = parseInt(el.id) - bX
+  const r = parseInt(el.id) + 1
+  const b = parseInt(el.id) + bX
+  const l = parseInt(el.id) - 1
+  if (t > -1 && same(el, t) && !isMarked(document.getElementById(t.toString()))) {
+    c++
+    mark(document.getElementById(t.toString()))
+    c += checkTRBL(document.getElementById(t.toString()))
+  }
+  if (r % bX !== 0 && same(el, r) && !isMarked(document.getElementById(r.toString()))) {
+    c++
+    mark(document.getElementById(r.toString()))
+    c += checkTRBL(document.getElementById(r.toString()))
+  }
+  if (b < bX * bY && same(el, b) && !isMarked(document.getElementById(b.toString()))) {
+    c++
+    mark(document.getElementById(b.toString()))
+    c += checkTRBL(document.getElementById(b.toString()))
+  }
+  if ((l + 1) % bX !== 0 && same(el, l) && !isMarked(document.getElementById(l.toString()))) {
+    c++
+    mark(document.getElementById(l.toString()))
+    c += checkTRBL(document.getElementById(l.toString()))
+  }
+  return c
+}
+
+function same (el, nb) {
+  return el.style.backgroundColor === $('#' + nb).css('backgroundColor')
+}
+function isMarked (el) {
+  return $(el).hasClass('mark')
+}
