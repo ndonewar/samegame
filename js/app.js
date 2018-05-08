68bbcc506d110bcb778c23bd81f40d61a2028dea
Neil Donewar <neil@donewar.com>
2018-05-08 15:31:45 -0400

    Use strict and refactor code out of global scope. Resize the board to original game's size.

--- js/app.js
+++ js/app.js
@@ -1,128 +1,139 @@
-/* global $ */
+/* global jQuery */
 
-const colors = ['Red', 'Blue', 'Green', 'Purple']
-const bX = 31
-const bY = 19
-let points = 0
-let total = 0
-function generatePieces () {
-  for (let i = 0; i < bX * bY; i++) {
-    const pTop = Math.floor(i / bX) * 40
-    const pLeft = (i % bX) * 40
-    const color = colors[Math.floor(Math.random() * colors.length)]
-    $('<div class="p"></div>').attr('id', i).css('top', pTop).css('left', pLeft).css('background-color', color).data('color', color).appendTo('#board').click(
-      function () {
-        if (isMarked(this)) {
-          deleteMarked()
-          settleDown()
-          settleLeft()
-          updateTotal(points)
+jQuery(function ($) {
+  'use strict'
+
+  const App = {
+
+    colors: ['Red', 'Blue', 'Green', 'Purple'],
+    bX: 25,
+    bY: 15,
+    points: 0,
+    total: 0,
+
+    init: function () {
+      for (let i = 0; i < this.bX * this.bY; i++) {
+        const pTop = Math.floor(i / this.bX) * 40
+        const pLeft = (i % this.bX) * 40
+        const color = this.colors[Math.floor(Math.random() * this.colors.length)]
+        $('<div class="p"></div>').attr('id', i).css('top', pTop).css('left', pLeft).css('background-color', color).data('color', color).appendTo('#board').click(this.handleTileClick.bind(this))
+      }
+    },
+
+    handleTileClick: function (event) {
+      const el = event.target
+      if (this.isMarked(el)) {
+        this.deleteMarked()
+        this.settleDown()
+        this.settleLeft()
+        this.updateTotal(this.points)
+      } else {
+        $('.mark').removeClass('mark')
+        let n = this.checkTRBL(el)
+        this.points = (n - 1) * (n - 1)
+        $('#points').html('' + this.points)
+        if (n) {
+          this.mark(el)
         } else {
-          $('.mark').removeClass('mark')
-          let n = checkTRBL(this)
-          points = (n - 1) * (n - 1)
-          $('#points').html('' + points)
-          if (n) {
-            mark(this)
-          } else {
-            updatePoints(0)
-          }
+          this.updatePoints(0)
         }
       }
-    )
-  }
-}
-generatePieces()
+    },
 
-function updatePoints (n) {
-  points = n
-  $('#points').html(n)
-}
+    updatePoints: function (n) {
+      this.points = n
+      $('#points').html(n)
+    },
 
-function updateTotal () {
-  total += points
-  $('#total').html('' + total)
-  updatePoints(0)
-}
+    updateTotal: function () {
+      this.total += this.points
+      $('#total').html('' + this.total)
+      this.updatePoints(0)
+    },
 
-function settleLeft () {
-  let again = false
-  for (let h = 0; h < bX - 1; h++) {
-    const col = []
-    for (let v = 0; v < bY; v++) {
-      const id = h + (bX * v)
-      if (document.getElementById(id.toString())) {
-        break
-      } else {
-        col.push(id)
-        if (col.length === bY) {
-          while (col.length > 0) {
-            const oid = col.pop()
-            if (document.getElementById(oid + 1)) {
-              again = true
-              $('#' + (oid + 1)).attr('id', oid).animate({left: '-=40'}, 10)
+    settleLeft: function () {
+      let again = false
+      for (let h = 0; h < this.bX - 1; h++) {
+        const col = []
+        for (let v = 0; v < this.bY; v++) {
+          const id = h + (this.bX * v)
+          if (document.getElementById(id.toString())) {
+            break
+          } else {
+            col.push(id)
+            if (col.length === this.bY) {
+              while (col.length > 0) {
+                const oid = col.pop()
+                if (document.getElementById(oid + 1)) {
+                  again = true
+                  $('#' + (oid + 1)).attr('id', oid).animate({left: '-=40'}, 10)
+                }
+              }
             }
           }
         }
       }
-    }
-  }
-  if (again) settleLeft()
-}
+      if (again) this.settleLeft()
+    },
 
-function settleDown () {
-  let again = false
-  for (let i = bX * bY - 1; i > -1; i--) {
-    if (!document.getElementById(i.toString())) {
-      if (document.getElementById((i - bX).toString())) {
-        $('#' + (i - bX)).attr('id', i).animate({top: '+=40'}, 10)
-        again = true
+    settleDown: function () {
+      let again = false
+      for (let i = this.bX * this.bY - 1; i > -1; i--) {
+        if (!document.getElementById(i.toString())) {
+          if (document.getElementById((i - this.bX).toString())) {
+            $('#' + (i - this.bX)).attr('id', i).animate({top: '+=40'}, 10)
+            again = true
+          }
+        }
       }
+      if (again) this.settleDown()
+    },
+
+    deleteMarked: function () {
+      $('.mark').remove()
+    },
+
+    mark: function (el) {
+      $(el).addClass('mark')
+    },
+
+    checkTRBL: function (el) {
+      let c = 0
+      const t = parseInt(el.id) - this.bX
+      const r = parseInt(el.id) + 1
+      const b = parseInt(el.id) + this.bX
+      const l = parseInt(el.id) - 1
+      if (t > -1 && this.same(el, t) && !this.isMarked(document.getElementById(t.toString()))) {
+        c++
+        this.mark(document.getElementById(t.toString()))
+        c += this.checkTRBL(document.getElementById(t.toString()))
+      }
+      if (r % this.bX !== 0 && this.same(el, r) && !this.isMarked(document.getElementById(r.toString()))) {
+        c++
+        this.mark(document.getElementById(r.toString()))
+        c += this.checkTRBL(document.getElementById(r.toString()))
+      }
+      if (b < this.bX * this.bY && this.same(el, b) && !this.isMarked(document.getElementById(b.toString()))) {
+        c++
+        this.mark(document.getElementById(b.toString()))
+        c += this.checkTRBL(document.getElementById(b.toString()))
+      }
+      if ((l + 1) % this.bX !== 0 && this.same(el, l) && !this.isMarked(document.getElementById(l.toString()))) {
+        c++
+        this.mark(document.getElementById(l.toString()))
+        c += this.checkTRBL(document.getElementById(l.toString()))
+      }
+      return c
+    },
+
+    same: function (el, nb) {
+      return $(el).data('color') === $('#' + nb).data('color')
+    },
+
+    isMarked: function (el) {
+      return $(el).hasClass('mark')
     }
   }
-  if (again) settleDown()
-}
 
-function deleteMarked () {
-  $('.mark').remove()
-}
-
-function mark (el) {
-  $(el).addClass('mark')
-}
-
-function checkTRBL (el) {
-  let c = 0
-  const t = parseInt(el.id) - bX
-  const r = parseInt(el.id) + 1
-  const b = parseInt(el.id) + bX
-  const l = parseInt(el.id) - 1
-  if (t > -1 && same(el, t) && !isMarked(document.getElementById(t.toString()))) {
-    c++
-    mark(document.getElementById(t.toString()))
-    c += checkTRBL(document.getElementById(t.toString()))
-  }
-  if (r % bX !== 0 && same(el, r) && !isMarked(document.getElementById(r.toString()))) {
-    c++
-    mark(document.getElementById(r.toString()))
-    c += checkTRBL(document.getElementById(r.toString()))
-  }
-  if (b < bX * bY && same(el, b) && !isMarked(document.getElementById(b.toString()))) {
-    c++
-    mark(document.getElementById(b.toString()))
-    c += checkTRBL(document.getElementById(b.toString()))
-  }
-  if ((l + 1) % bX !== 0 && same(el, l) && !isMarked(document.getElementById(l.toString()))) {
-    c++
-    mark(document.getElementById(l.toString()))
-    c += checkTRBL(document.getElementById(l.toString()))
-  }
-  return c
-}
-
-function same (el, nb) {
-  return $(el).data('color') === $('#' + nb).data('color')
-}
-function isMarked (el) {
-  return $(el).hasClass('mark')
-}
+  App.init()
+})
